483287028aa1c905c1cc93067e57f25b734761d0
paninizer <101371138+paninizer@users.noreply.github.com>
2024-01-10 15:13:52 -0500

    templating
    creating basic structure for quickstart, will return with more commands / features
    
    great framework mhm

---
+++ src/commands/mod.rs
@@ -0,0 +1,6 @@
+mod math;
+mod info;
+mod neko;
+mod setup;
+mod settings;
+mod utilities;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use poise::serenity_prelude as serenity;
+
+struct Data {} // User data, which is stored and accessible in all command invocations
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+
+/// Displays your or another user's account creation date
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<serenity::User>,
+) -> Result<(), Error> {
+    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", u.name, u.created_at());
+    ctx.say(response).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().expect("Failed to load .env file");
+    // gets token, exits if no token
+    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+    // Initialize the logger to use environment variables.
+    //
+    // In this case, a good default is setting the environment variable `RUST_LOG` to `debug`.
+    tracing_subscriber::fmt::init();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age()],
+            ..Default::default()
+        })
+        .token(token)
+        .intents(serenity::GatewayIntents::all())
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        });
+
+    framework.run_autosharded().await.unwrap();
+
+
 }
