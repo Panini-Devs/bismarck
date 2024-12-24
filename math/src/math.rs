328334206f0244c521f04e4e6479d8912d1f1ccd
Aimar Ibarra <aimar_ibarra@proton.me>
2024-12-24 13:23:18 +0100

    Migrated traits to structs
    It makes more sense, both in a design and efficiency context, to use
    structs instead of traits

--- math/src/math.rs
+++ math/src/math.rs
@@ -1,6 +1,6 @@
 
-/// Trait describing a mathematical operator that takes two operands
-trait BinaryOperator {
+/// Struct describing a mathematical operator that takes two operands
+struct BinaryOperator {
     /// The precedence of an determines where it is located in the priority chain.
     ///
     /// Multiplication has higher precedence than addition,
@@ -8,12 +8,12 @@ trait BinaryOperator {
     ///
     /// A limitation is imposed: All unary operators have higher precedence than any binary operator
     /// no matter how higher is the precedence value of the binary operator.
-    const PRECEDENCE: i32;
+    precedence: i32,
 
     /// The identifier of the operator, not limited to just one character.
     /// A symbol can be defined as an operator, such that `5 and 1` would evaluate to `1`
     /// (boolean addition)
-    const IDENTIFIER: &'static str;
+    identifier: &'static str,
 
     /// Operator application method
     ///
@@ -27,13 +27,13 @@ trait BinaryOperator {
     /// Result of applying the operator to the two operands.
     /// The return type is a `Result<f64, &'static str>` for the application may error,
     /// for example in cases where division by zero happens.
-    fn apply(&self, left_operand: f64, right_operand: f64) -> Result<f64, &'static str>;
+    apply: fn(left_operand: f64, right_operand: f64) -> Result<f64, &'static str>,
 }
 
-/// Trait describing a mathematical operator that takes one operand
-trait UnaryOperator {
+/// Struct describing a mathematical operator that takes one operand
+struct UnaryOperator {
     /// Identifier of the operator 
-    const IDENTIFIER: char;
+    identifier: char,
 
     /// Operator application method
     ///
@@ -45,5 +45,5 @@ trait UnaryOperator {
     ///
     /// Result of applying the operator to the operand.
     /// One common error to signal is overflow, for example, caused by `50!`.
-    fn apply(&self, operand: f64) -> Resutl<f64, &'static str>;
+    apply: fn(operand: f64) -> Result<f64, &'static str>,
 }
